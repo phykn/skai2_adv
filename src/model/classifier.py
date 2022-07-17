3a3f5a5016d30d5ace7140de6a4b09bc7e5633cc
Kwangnam Yu <53140105+phykn@users.noreply.github.com>
2022-07-18 02:10:06 +0900

    Update classifier.py

--- src/model/classifier.py
+++ src/model/classifier.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 from typing import Dict
 from timm.loss import LabelSmoothingCrossEntropy
 from .convnext import convnext_tiny
-from .layers import ListLayerNorm, ImageGenerator, PositionEmbeddingSine, ChangeKernel, GlobalAveragePooling2D, AttentionEncodeLayer
+from .layers import ListLayerNorm, ChangeKernel
+from .transformer import PositionEmbedding1D, PositionEmbedding2D, Transformer
 
 
 class Classifier(nn.Module):
@@ -13,177 +14,100 @@ class Classifier(nn.Module):
         num_class: int=5,
         pretrained: bool=True,
         drop_path_rate: float=0.1,
-        num_encode_layer: int=3,
-        hid_dim: int=256,
-        num_head: int=8,
         dropout: float=0.1,
+        d_model: int=256,
+        n_head: int=8, 
+        num_encoder_layers: int=6,
+        num_decoder_layers: int=6, 
+        d_ff: int=1024,
         label_smoothing: float=0.1
     ) -> None:
         super().__init__()
-        self.kernels = [768, 384, 192, 96, 64, 3]
-
-        self.convnext = convnext_tiny(pretrained=True, drop_path_rate=0.1)
-        self.norm = ListLayerNorm(self.kernels[:4][::-1])
-
-        self.img_gen = ImageGenerator(self.kernels)
-
-        self.cng_kernel = ChangeKernel(self.kernels[0], hid_dim)
-        self.pos_encode = PositionEmbeddingSine(num_pos_feats=hid_dim//2, normalize=True)
-        self.att_encode = nn.Sequential(
-            *[AttentionEncodeLayer(d_model=hid_dim, n_head=num_head, d_ff=hid_dim*4, dropout=dropout) for _ in range(num_encode_layer)]
+        num_kernels = [96, 192, 384, 768]
+        self.convnext = convnext_tiny(
+            pretrained=True, 
+            drop_path_rate=drop_path_rate
+        )        
+        self.norm = ListLayerNorm(num_kernels)
+        self.change = ChangeKernel(
+            in_channels=num_kernels[-1], 
+            out_channels=d_model
         )
-
-        self.pooling = GlobalAveragePooling2D(hid_dim)
-        self.head = nn.Linear(hid_dim, num_class)
-
+        
+        self.pe_1d = PositionEmbedding1D()        
+        self.pe_2d = PositionEmbedding2D(
+            num_pos_feats=d_model//2,
+            normalize=True
+        )
+        self.embed = nn.Embedding(
+            num_embeddings=num_class, 
+            embedding_dim=d_model
+        )
+        nn.init.xavier_uniform_(self.embed.weight)
+        
+        self.transformer = Transformer(
+            d_model=d_model,
+            n_head=n_head, 
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers, 
+            d_ff=d_ff, 
+            dropout=dropout
+        )
+        
+        self.classifier = nn.Linear(
+            in_features=d_model,
+            out_features=2
+        )
+        nn.init.xavier_uniform_(self.classifier.weight)
+        nn.init.constant_(self.classifier.bias, 0)
+        
         self.ce_loss = LabelSmoothingCrossEntropy(label_smoothing)
-        self.l1_loss = nn.L1Loss()
+        
+    @staticmethod
+    def bcwh2nbc(
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        return x.flatten(2).permute(2, 0, 1)
 
     def forward(
         self,
-        x: torch.Tensor
+        x: torch.Tensor,
+        y: torch.Tensor
     ) -> torch.Tensor:
         # convnext
         xs = self.convnext(x)
         xs = self.norm(xs)
-
+        x = xs[-1]
+        
+        # x, p
+        x = self.change(x)
+        p = self.pe_2d(x)
+        
+        x = self.bcwh2nbc(x)
+        p = self.bcwh2nbc(p)
+        
         # add noise
         if self.training:
-            xs[-1] = xs[-1] + torch.randn_like(xs[-1])
-
-        # generate image
-        image = self.img_gen(xs)
-
-        # attention
-        x = xs[-1]
-        x = self.cng_kernel(x)
-        p = self.pos_encode(x)
-        for encoder in self.att_encode:
-            x, score = encoder(x, p)
-
-        # pooling
-        embed = self.pooling(x)
-
-        # logit
-        logit = self.head(embed)
-
-        return dict(
-            logit=logit,
-            image=image,
-            score=score,
-            embed=embed
-        )
-   
+            x = x + torch.randn_like(x)
+            
+        # y, q
+        y = self.embed(y).permute(1, 0, 2)
+        q = self.pe_1d(y)
+        
+        # transformer
+        o = self.transformer(x, p, y, q)
+        o = self.classifier(o)
+        return o
+    
     def loss(
         self,
         data: Dict[str, torch.Tensor]
-    ) -> Dict[str, torch.Tensor]:
-        out = self(data["input_image"])
-        ce_loss = self.ce_loss(out["logit"], data["label"])
-        l1_loss = self.l1_loss(out["image"], data["target_image"])
-        return dict(
-            clf_loss=ce_loss, 
-            img_loss=l1_loss, 
-            loss=ce_loss+l1_loss
-        )
-
-    def twin(
-        self,
-        data: Dict[str, torch.Tensor]
-    ) -> Dict[str, torch.Tensor]:
-        out = self(data["input_image"])
-        ce_loss = self.ce_loss(out["logit"], data["label"])
-        l1_loss = self.l1_loss(out["image"], data["target_image"])
-        accuracy = np.mean(
-            torch.argmax(out["logit"], axis=1).detach().cpu().numpy()==data["label"].cpu().numpy()
-        )
-
-        return dict(
-            clf_loss=ce_loss,
-            img_loss=l1_loss,
-            loss=(ce_loss+l1_loss)/2,
-            embed=out["embed"],
-            accuracy=torch.tensor(accuracy)
-        )
-
-
-class Twin_Classifier(nn.Module):
-    def __init__(
-        self,
-        num_class: int=5,
-        pretrained: bool=True,
-        drop_path_rate: float=0.1,
-        num_encode_layer: int=3,
-        hid_dim: int=256,
-        num_head: int=8,
-        dropout: float=0.1,
-        label_smoothing: float=0.1
-    ) -> None:
-        super().__init__()
-        self.clf = Classifier(
-            num_class=num_class,
-            pretrained=pretrained,
-            drop_path_rate=drop_path_rate,
-            num_encode_layer=num_encode_layer,
-            hid_dim=hid_dim,
-            num_head=num_head,
-            dropout=dropout,
-            label_smoothing=label_smoothing
-        )
-        self.pair = nn.Linear(3*hid_dim, 2, bias=True)
-        self.ce_loss = LabelSmoothingCrossEntropy(label_smoothing)
-
-    def forward(
-        self,
-        x0: torch.Tensor,
-        x1: torch.Tensor
     ) -> torch.Tensor:
-        return dict(
-            y0=self.clf(x0),
-            y1=self.clf(x1)
-        )
-
-    def predict(
-        self,
-        data: Dict[str, Dict[str, torch.Tensor]]
-    ) -> Dict[str, torch.Tensor]:
-        return self(
-            data["data_0"]["input_image"],
-            data["data_1"]["input_image"]
-        )
-
-    def loss(
-        self,
-        data: Dict[str, Dict[str, torch.Tensor]]
-    ) -> Dict[str, torch.Tensor]:
-        data_0 = data["data_0"]
-        data_1 = data["data_1"]
-
-        twin_0 = self.clf.twin(data_0)
-        twin_1 = self.clf.twin(data_1)
-
-        p_pair = self.pair(
-            torch.cat([
-                twin_0["embed"],
-                twin_1["embed"],
-                torch.abs(twin_0["embed"]-twin_1["embed"])
-            ], axis=1)
-        )
-        t_pair = 1*(data["data_0"]["label"]==data["data_1"]["label"])
-
-        pair_loss = self.ce_loss(p_pair, t_pair)
-
-        pair_acc = np.mean(
-            torch.argmax(p_pair, axis=1).detach().cpu().numpy()==t_pair.cpu().numpy()
-        )
-        return dict(
-            clf_loss_0=twin_0["clf_loss"],
-            img_loss_0=twin_0["img_loss"],
-            accuracy_0=twin_0["accuracy"],
-            clf_loss_1=twin_1["clf_loss"],
-            img_loss_1=twin_1["img_loss"],
-            accuracy_1=twin_1["accuracy"],
-            pair_loss=pair_loss,
-            pair_acc=torch.tensor(pair_acc),
-            loss=(twin_0["loss"]+twin_1["loss"]+pair_loss)/3
-        )
+        x = data["image"]
+        y = data["query"]
+        t = data["label"]
+        
+        o = self(x, y).flatten(0, 1)
+        t = t.flatten(0)        
+        loss = self.ce_loss(o, t)
+        return dict(loss=loss)
