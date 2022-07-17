454b0e1df90457f4c20721811e9cb0557af174e1
Kwangnam Yu <53140105+phykn@users.noreply.github.com>
2022-07-18 00:46:56 +0900

    Update dataset.py

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 import albumentations as A
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from .open_image import open_image
 from .misc import get_unique_indices
 
@@ -12,30 +12,52 @@ class CLF_Dataset:
         files: List[str],
         labels: List[int],
         img_size: int=224,
-        random_select: bool=False,
-        test: bool=False
+        test: bool=False,
+        background_files: Optional[List[str]]=None
     ) -> None:
         self.files = files
         self.labels = labels
-        self.random_select = random_select
         self.test = test
+        self.background_files = background_files
 
         unique, self.indices = get_unique_indices(labels)
         self.num_class = len(unique)
+        self.setup_transform(img_size=img_size)
 
-        r_transform = [
-            A.Resize(
-                height=img_size, 
-                width=img_size
-            )
-        ]
-        self.r_transform = A.Compose(r_transform)
+    def __len__(
+        self
+    ) -> int:
+        return len(self.files)
 
-        b_transform = [
-            A.Resize(
-                height=img_size, 
-                width=img_size
-            ),
+    def __getitem__(
+        self,
+        idx: int
+    ) -> dict:
+        image, label = self.get_data()            
+        return dict(image=image, label=label)
+    
+    def get_random_idx(
+        self
+    ) -> int:
+        class_id = np.random.choice(range(0, self.num_class))
+        return random.choice(self.indices[class_id])
+
+    def setup_transform(
+        self,
+        img_size: int=224
+    ) -> None:
+        self.resize = A.Resize(
+            height=img_size,
+            width=img_size
+        )
+
+        self.random_crop = A.RandomSizedCrop(
+            min_max_height=(img_size//2, img_size*4),
+            height=img_size,
+            width=img_size
+        )
+
+        self.reshape = A.Compose([
             A.HorizontalFlip(
                 p=0.5
             ),
@@ -51,19 +73,25 @@ class CLF_Dataset:
                 value=0,
                 p=0.5
             ),
-            A.RandomBrightnessContrast(
-                brightness_limit=0.2, 
-                contrast_limit=0.2, 
-                p=0.5
-            ),
             A.ElasticTransform(
-                border_mode=0, 
+                border_mode=0,
                 p=0.5
             )
-        ]
-        self.b_transform = A.Compose(b_transform)
+        ])
 
-        n_transform = [
+        self.recolor = A.Compose([
+            A.RandomBrightnessContrast(
+                brightness_limit=0.2,
+                contrast_limit=0.2,
+                p=0.5
+            ),
+            A.Solarize(
+                threshold=[88, 168],
+                p=0.5
+            )
+        ])
+
+        self.noise = A.Compose([
             A.Blur(
                 blur_limit=7, 
                 p=0.5
@@ -71,38 +99,79 @@ class CLF_Dataset:
             A.GaussNoise(
                 p=0.5
             ),
-            A.OneOf(
-                [
-                    A.CoarseDropout(
-                        max_height=112,
-                        max_width=112,
-                        max_holes=4,
-                        min_height=32,
-                        min_width=32,
-                        min_holes=1,
-                        p=1.0
-                    ),
-                    A.ToGray(
-                        p=1.0
-                    ),
-                    A.Solarize(
-                        threshold=[88, 168],
-                        p=1.0
-                    )
-                ],
+            A.CoarseDropout(
+                max_height=56,
+                max_width=56,
+                max_holes=8,
+                min_height=28,
+                min_width=28,
+                min_holes=4,
                 p=0.5
             )
-        ]
-        self.n_transform = A.Compose(n_transform)
-
-        self.normalize = A.Compose([
-            A.Normalize(
-                mean=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225),
-                max_pixel_value=255.0,
-                p=1.0
-            )
         ])
+
+        self.normalize = A.Normalize(
+            mean=(0.485, 0.456, 0.406),
+            std=(0.229, 0.224, 0.225),
+            max_pixel_value=255.0,
+            p=1.0
+        )
+
+    def transform_image(
+        self,
+        image: np.ndarray
+    ) -> Tuple[np.ndarray]:
+        if not self.test:
+            image = self.reshape(image=image)["image"]
+            image = self.recolor(image=image)["image"]
+            image = self.noise(image=image)["image"]
+        image = self.normalize(image=image)["image"]
+        image = image.transpose(2, 0, 1)
+        return image
+    
+    def get_data(
+        self
+    ) -> Tuple[np.ndarray, int]:
+        idx = self.get_random_idx()            
+        if self.background_files is not None:
+            if np.random.rand() < 1/(self.num_class+1):
+                label = -1
+                image = open_image(
+                    np.random.choice(self.background_files)
+                )
+                image = self.random_crop(image=image)["image"]
+            else:
+                label = self.labels[idx]
+                image = open_image(self.files[idx])
+                image = self.resize(image=image)["image"]
+            label = label + 1
+
+        else:
+            label = self.labels[idx]
+            image = open_image(self.files[idx])
+            image = self.resize(image=image)["image"]
+            
+        image = self.transform_image(image)
+        return image, label
+
+
+class Twin_CLF_Dataset(CLF_Dataset):
+    def __init__(
+        self,
+        files: List[str],
+        labels: List[int],
+        img_size: int=224,
+        test: bool=False,
+        background_files: Optional[List[str]]=None
+    ) -> None:
+        self.files = files
+        super().__init__(
+            files=files, 
+            labels=labels, 
+            img_size=img_size,
+            test=test,
+            background_files=background_files            
+        )
 
     def __len__(
         self
@@ -113,111 +182,24 @@ class CLF_Dataset:
         self,
         idx: int
     ) -> dict:
-        if self.random_select:
-            idx = self._get_random_idx()
-
-        file = self.files[idx]
-        label = self.labels[idx]
-
-        image = open_image(file)
-        input_image, target_image = self.transform_image(image)
-
+        image_1, label_1 = self.get_data()
+        image_2, label_2 = self.get_data()
         return dict(
-            input_image=input_image,
-            target_image=target_image,
-            label=label
+            image_1=image_1,
+            label_1=label_1,
+            image_2=image_2,
+            label_2=label_2,
+            compare=1*(label_1==label_2)
         )
 
-    def _get_random_idx(
-        self
-    ) -> int:
-        l = np.random.choice(range(0, self.num_class), size=1, replace=False)[0]
-        return random.choice(self.indices[l])
 
-    def transform_image(
-        self,
-        image: np.ndarray
-    ) -> Tuple[np.ndarray]:
-        if self.test:
-            target_image = self.r_transform(image=image)["image"]
-            input_image = target_image.copy()
-        else:
-            target_image = self.b_transform(image=image)["image"]
-            input_image = self.n_transform(image=target_image)["image"]
-
-        target_image = self.normalize(image=target_image)["image"].transpose(2, 0, 1)
-        input_image = self.normalize(image=input_image)["image"].transpose(2, 0, 1)
-
-        return input_image, target_image
-
-
-class Twin_CLF_Dataset(CLF_Dataset):
-    def __init__(
-        self,
-        files: List[str],
-        labels: List[int],
-        img_size: int=224,
-        test: bool=False
-    ) -> None:
-        self.files = files
-        super().__init__(
-            files=files, 
-            labels=labels, 
-            img_size=img_size, 
-            test=test
-        )
-
-    def __len__(
-        self
-    ) -> int:
-        return 2*len(self.files)
-
-    def _get_random_twin_idx(
-        self
-    ) -> Tuple[int]:
-        same_label = random.choice([False, True])
-        if same_label:
-            l = np.random.choice(range(0, self.num_class), size=1, replace=False)[0]
-            return random.choice(self.indices[l]), random.choice(self.indices[l])
-        else:
-            l0, l1 = np.random.choice(range(0, self.num_class), size=2, replace=False)
-            return random.choice(self.indices[l0]), random.choice(self.indices[l1])
-
-    def __getitem__(
-        self,
-        idx: int
-    ) -> dict:
-        idx_0, idx_1 = self._get_random_twin_idx()
-
-        file_0 = self.files[idx_0]
-        label_0 = self.labels[idx_0]
-        input_image_0, target_image_0 = self.transform_image(open_image(file_0))
-
-        file_1 = self.files[idx_1]
-        label_1 = self.labels[idx_1]
-        input_image_1, target_image_1 = self.transform_image(open_image(file_1))
-
-        return dict(
-            data_0=dict(
-                input_image=input_image_0,
-                target_image=target_image_0,
-                label=label_0
-            ),
-            data_1=dict(
-                input_image=input_image_1,
-                target_image=target_image_1,
-                label=label_1
-            )
-        )
-    
-    
 class Test_Dataset:
     def __init__(
         self,
         files: List[str],
         img_size: int=224,
     ) -> None:
-        self.files = files        
+        self.files = files
         self.transform = A.Compose([
             A.Resize(
                 height=img_size, 
@@ -244,6 +226,4 @@ class Test_Dataset:
         image = open_image(file)
         image = self.transform(image=image)["image"].transpose(2, 0, 1)
 
-        return dict(
-            input_image=image
-        )
+        return dict(image=image)
